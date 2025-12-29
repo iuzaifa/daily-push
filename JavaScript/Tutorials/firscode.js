a55d36d6d3d5497f3fe11db196b0379cf3754929
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-12-30 00:26:44 +0530

    update closure of js

--- JavaScript/Tutorials/firscode.js
+++ JavaScript/Tutorials/firscode.js
@@ -1,8 +1,26 @@
 
 
+// my first js code 
 function add(a, b){
     return a + b;
 }
-
 console.log(add(3,5));
+
+function sum(a){
+    return function (b) {
+      return a + b;
+    };
+}
+
+console.log(sum(2)(3))
+
+function name(firstname){
+    return function (lastname){
+        return firstname + " "+ lastname;
+    }
+}
+let fname = "Abu";
+let lname = "Huzaifa";
+console.log(name(fname)(lname))
+console.log(name(fname)(''))
 
