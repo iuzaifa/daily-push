f3e53002b57f6882dd6264b89758e915014c5390
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2026-01-14 20:19:07 +0530

    complete MUI Fist project

--- Material-UI/material-ui-project-01/src/App.jsx
+++ Material-UI/material-ui-project-01/src/App.jsx
@@ -1,27 +1,41 @@
-import React from 'react'
-import Buttons from './components/mui/Buttons';
+import React, { useState } from "react";
+import Buttons from "./components/mui/Buttons";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import MyApp from "./components/mui/MyApp"
-
-
-const theme = createTheme({
-  colorSchemes: {
-    dark: true,
-  },
-});
+import MyApp from "./components/mui/MyApp";
+import ThemeUISwitch from "./components/mui/ThemeUISwitch";
+import { useMemo } from "react";
+import ResponsiveAppBar from "./components/mui/ResponsiveAppBar";
 
 
 const App = () => {
+
+  const [mode, setMode] = useState("light");
+
+   const theme = useMemo(
+     () =>
+       createTheme({
+         palette: {
+           mode: mode,
+         },
+       }),
+     [mode]
+   );
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Buttons />
-        <MyApp />
-      </ThemeProvider>
+      <ResponsiveAppBar/>
+      <div className="aapp">
+        <ThemeProvider theme={theme}>
+
+          <CssBaseline />
+          <Buttons />
+          <MyApp />
+
+          <ThemeUISwitch mode={mode} setMode={setMode} />
+        </ThemeProvider>
+      </div>
     </>
   );
-}
+};
 
-export default App
+export default App;
