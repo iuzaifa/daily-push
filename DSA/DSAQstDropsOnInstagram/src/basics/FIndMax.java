ed97d493c3c0e4ca7c8d0c862ffe26358cc9f6fe
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2026-02-11 23:01:49 +0530

    update

--- DSA/DSAQstDropsOnInstagram/src/basics/FIndMax.java
+++ DSA/DSAQstDropsOnInstagram/src/basics/FIndMax.java
@@ -2,7 +2,7 @@ package basics;
 
 public class FIndMax {
     static void main() {
-        int []  ar  = {7,789 ,12, 9, 4, 11, 99, 00, 88, 9, 567};
+        int []  ar  = {7,789 ,12, 9, 4, 11, 99, 0, 88, 9, 567};
         System.out.println(max(ar));
 
     }
@@ -10,9 +10,9 @@ public class FIndMax {
     private static int max(int[] ar) {
 
         int mx = ar[0];
-        for (int i =0; i < ar.length; i++){
-            if (ar[i] > mx){
-                mx = ar[i];
+        for (int j : ar) {
+            if (j > mx) {
+                mx = j;
             }
         }
 
